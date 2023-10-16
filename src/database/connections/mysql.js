773ca83018e2a03fc03288905d9ce9822a000137
Scott Minter <sminter@macbook-pro.mynetworksettings.com>
2023-10-16 09:08:57 -0400

    moved conn data to .env file

--- src/database/connections/mysql.js
+++ src/database/connections/mysql.js
@@ -1,14 +1,15 @@
 "use static";
 
 const mysql = require("mysql2/promise");
+const configs = require("dotenv").config().parsed;
 
 const mysqlConnection = async () => {
   return await mysql.createConnection({
-    host: "",
-    port: "",
-    user: "",
-    password: "",
-    database: "",
+    host: configs.DB_HOST,
+    port: configs.DB_PORT,
+    user: configs.DB_USER,
+    password: configs.DB_PW,
+    database: configs.DB_SCHEMA,
     Promise: Promise,
   });
 };
